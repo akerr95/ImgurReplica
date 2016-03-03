2fc9565476a925554c97b928f83f8e52136830b2
Alec Kerr <akerr_95@hotmail.com>
2016-03-02 20:44:56 -0500

    This commit is based on adding crud to the controllers , stops at the comment script

--- controllers/image.js
+++ controllers/image.js
@@ -1,81 +1,149 @@
 var fs = require('fs'),
-    path = require('path'),
-    sidebar = require('../helpers/sidebar'),
-    express = require('express'),
-      router = express.Router(),
-      multer  = require('multer'),
-    upload = multer({ dest: 'public/upload/temp' });
+  path = require('path'),
+  sidebar = require('../helpers/sidebar'),
+  express = require('express'),
+  router = express.Router(),
+  Models = require('../models'),
+  multer = require('multer'),
+  md5 = require('md5'),
+  upload = multer({
+    dest: 'public/upload/temp'
+  });
 
 
-router.get('/:image_id', function(req, res){
+router.get('/:image_id', function(req, res) {
   var viewModel = {
-      image: {
-          uniqueId:       1,
-          title:          'Sample Image 1',
-          description:    'This is a sample.',
-          filename:       'sample1.jpg',
-          views:          0,
-          likes:          0,
-          timestamp:      Date.now
-      },
-      comments: [
-          {
-              image_id:   1,
-              email:      'test@testing.com',
-              name:       'Test Tester',
-              gravatar:   'http://www.gravatar.com/avatar/9a99fac7b524fa443560ec7b5ece5ca1?d=monsterid&s=45',
-              comment:    'This is a test comment...',
-              timestamp:  Date.now()
-          },{
-              image_id:   1,
-              email:      'test@testing.com',
-              name:       'Test Tester',
-              gravatar:   'http://www.gravatar.com/avatar/9a99fac7b524fa443560ec7b5ece5ca1?d=monsterid&s=45',
-              comment:    'Another followup comment!',
-              timestamp:  Date.now()
-          }
-      ]
+    image: {},
+    comments: {}
   };
 
-  sidebar(viewModel, function(viewModel) {
-      res.render('image', viewModel);
-  });
-});
-router.post('/',upload.single('file'), function(req, res){
-  var saveImage = function() {
-      var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
-          imgUrl = '';
-
-      for(var i=0; i < 6; i+=1) {
-          imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
+  Models.Image.findOne({
+      filename: {
+        $regex: req.params.image_id
       }
+    },
+    function(err, image) {
+      if (err) {
+        throw err;
+      }
+      if (image) {
+        image.views = image.views + 1;
+        viewModel.image = image;
+        image.save();
+        Models.Comment.find({
+            image_id: image._id
+          }, {}, {
+            sort: {
+              'timestamp': 1
+            }
+          },
+          function(err, comments) {
+            if (err) {
+              throw err;
+            }
 
-      var tempPath = req.file.path,
+            viewModel.comments = comments;
+
+            sidebar(viewModel, function(viewModel) {
+              res.render('image', viewModel);
+            });
+          }
+        );
+      } else {
+        res.redirect('/');
+      }
+    });
+});
+router.post('/', upload.single('file'), function(req, res) {
+  var saveImage = function() {
+    var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
+      imgUrl = '';
+
+    for (var i = 0; i < 6; i += 1) {
+      imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    Models.Image.find({
+      filename: imgUrl
+    }, function(err, images) {
+      if (images.length > 0) {
+        saveImage();
+      } else {
+        var tempPath = req.file.path,
           ext = path.extname(req.file.originalname).toLowerCase(),
           targetPath = path.resolve('./public/upload/' + imgUrl + ext);
 
-      if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+        if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
           fs.rename(tempPath, targetPath, function(err) {
-              if (err) throw err;
-
-              res.redirect('/images/' + imgUrl);
+            if (err) throw err;
+            var newImg = new Models.Image({
+              title: req.body.title,
+              filename: imgUrl + ext,
+              description: req.body.description
+            });
+            newImg.save(function(err, image) {
+              res.redirect('/images/' + image.uniqueId);
+            });
           });
-      } else {
-          fs.unlink(tempPath, function (err) {
-              if (err) throw err;
+        } else {
+          fs.unlink(tempPath, function(err) {
+            if (err) throw err;
 
-              res.status(500).json({error:'Only image files are allowed'});//res.json(500, {error: 'Only image files are allowed.'});
+            res.status(500).json({
+              error: 'Only image files are allowed'
+            }); //res.json(500, {error: 'Only image files are allowed.'});
           });
+        }
       }
+    });
+
   };
 
   saveImage();
 });
 router.post('/:image_id/like', function(req, res) {
-    res.json({likes: 1});
+  Models.Image.findOne({
+      filename: {
+        $regex: req.params.image_id
+      }
+    },
+    function(err, image) {
+      if (!err && image) {
+        image.likes = image.likes + 1;
+        image.save(function(err) {
+          if (err) {
+            res.json(err);
+          } else {
+            res.json({
+              likes: image.likes
+            });
+          }
+        });
+      }
+    });
 });
-router.post('/:image_id/comment', function(req, res){
-    res.send('The image:comment POST controller');
+router.post('/:image_id/comment', function(req, res) {
+  Models.Image.findOne({
+      filename: {
+        $regex: req.params.image_id
+      }
+    },
+    function(err, image) {
+      if (!err && image) {
+        var newComment = new Models.Comment(req.body);
+        newComment.gravatar = md5(newComment.email);
+        newComment.image_id = image._id;
+        newComment.save(function(err, comment) {
+          if (err) {
+            throw err;
+          }
+
+          res.redirect('/images/' + image.uniqueId + '#' + comment._id);
+        });
+      } else {
+        res.redirect('/');
+      }
+    });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 var express = require('express'),
   config = require('./configure'),
+  mongoose = require('mongoose');
   app = express();
 app.set('port', process.env.PORT || 3300);
 app.set('views','./views');
 app = config(app);
+mongoose.connect('mongodb://localhost/imgPloadr');
+mongoose.connection.on('open', function() {
+console.log('Mongoose connected.');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Server up: http://localhost:' + app.get('port'));
