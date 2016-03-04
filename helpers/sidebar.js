c01f58d07ccee086dfd9ed52e5a6c1a546406530
Alec Kerr <akerr_95@hotmail.com>
2016-03-04 08:29:39 -0500

    Updated the database to incorporate mongoose and also async module

--- helpers/sidebar.js
+++ helpers/sidebar.js
@@ -1,13 +1,28 @@
 var Stats = require('./stats'),
-    Images = require('./images'),
-    Comments = require('./comments');
+  Images = require('./images'),
+  async = require('async');
+Comments = require('./comments');
 
-module.exports = function(viewModel, callback){
-    viewModel.sidebar = {
-        stats: Stats(),
-        popular: Images.popular(),
-        comments: Comments.newest()
-    };
+module.exports = function(viewModel, callback) {
+  async.parallel([
+    function(next) {
+       Stats(next);
+    },
+    function(next) {
+      Images.popular(next);
+    },
+    function(next) {
+      Comments.newest(next);
+    }
+  ],
+    function(err, results) {
+    console.log(results);
+      viewModel.sidebar = {
+        stats: results[0],
+        popular: results[1],
+        comments: results[2]
+      };
 
-    callback(viewModel);
+      callback(viewModel);
+    });
 };
